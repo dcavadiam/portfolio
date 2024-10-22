0a7ac5f73adcc6dbcc7fa577f7c34cd2720ab5ee
Diego <dcavadia@uninorte.edu.co>
2024-10-22 14:07:54 -0500

    fix tailwind theme and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,23 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			transparent: 'transparent',
-			'jacaranda': {
-				'50': '#fef3ff',
-				'100': '#fde7ff',
-				'200': '#fcceff',
-				'300': '#fca7ff',
-				'400': '#fb72ff',
-				'500': '#f33df8',
-				'600': '#da1ddc',
-				'700': '#b714b4',
-				'800': '#951392',
-				'900': '#7a1576',
-				'950': '#2a0028',
+		extend: {
+			colors: {
+				transparent: 'transparent',
+				'jacaranda': {
+					'50': '#fef3ff',
+					'100': '#fde7ff',
+					'200': '#fcceff',
+					'300': '#fca7ff',
+					'400': '#fb72ff',
+					'500': '#f33df8',
+					'600': '#da1ddc',
+					'700': '#b714b4',
+					'800': '#951392',
+					'900': '#7a1576',
+					'950': '#2a0028',
+				},
 			},
 		},
+
 	},
 	plugins: [],
 }
