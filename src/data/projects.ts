6d99d9978e584b8f80294f19ca26773c9c79a156
dcavadia <dcavadia@uninorte.edu.co>
2025-06-06 09:33:17 -0500

    🎨 Actualización: se añade el proyecto Tasky App, ademas se añade la funcionalidad "Coming Soon" en el componente ProjectCard.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,23 @@ import type { Project } from "@custom-types/project";
 
 export const projects: Project[] = [
   {
+    title: "Taski App",
+    description:
+      "Taski App is a task management application that allows you to create, edit, and delete tasks. It also allows you to mark tasks as completed and delete them.",
+    image: "/taskiapp.webp",
+    technologies: ["Next", "Tailwind", "Typescript", "ShadCN"],
+    links: [
+      {
+        name: "Code",
+        url: "https://github.com/dcavadiam/taski-app",
+      },
+      {
+        name: "Deploy",
+        url: "https://taski-app.vercel.app/",
+      },
+    ],
+  },
+  {
     title: "User Product Manager",
     description:
       "The application provides a dashboard with two main sections: User Management, which allows for viewing and basic management of a list of users, and Product Management, which enables the viewing and basic management of a list of products. Both sections offer simple tools for efficient administration of users and products.",
@@ -32,23 +49,6 @@ export const projects: Project[] = [
       {
         name: "Deploy",
         url: "https://blossom-test-dcavadia.vercel.app/",
-      },
-    ],
-  },
-  {
-    title: "4 in line",
-    description:
-      "This is a simple game where you have to make a line of 4 matching colors without your opponent being able to do so.",
-    image: "/cuatro-en-linea.webp",
-    technologies: ["Vite", "Javascript"],
-    links: [
-      {
-        name: "Code",
-        url: "https://github.com/dcavadiam/react/tree/main/self-learning/cuatro-en-linea",
-      },
-      {
-        name: "Deploy",
-        url: "https://cm-fourinline.netlify.app",
       },
     ],
   },
